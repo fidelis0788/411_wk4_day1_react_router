7a26f2d7018082c793182c033c55b934fb0bdbcb
Apple <apple@Fideles-Mac-Pro.attlocal.net>
2020-12-13 22:25:55 -0600

    initial commit react-router

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,12 +1,36 @@
-import React from 'react'
-import cars from '../cars.json'
-// import material ui components here //
-// Container, Paper, Chip //
+import React from "react";
+import cars from "../cars.json";
+ import { Container, Paper, Chip } from "@material-ui/core";
+import "../App.css";
+
 
 const Car = (props) => {
+    const id = props.match.params.id;
+    const car = cars.find(car => car.id ==id);
+    // console.log(id)
+    console.log(car)
     return (
-        <h1>A specific car</h1>
+        <div>
+            <Container maxWidth="sm" className="car-container">
+                <Paper className="car-paper">
+                    <div>
+                    { <h2 style={{ textTransform: 'uppercase'}} >{car.Name}</h2>}
+                    </div>
+                    <div>
+                        <Chip size="medium" label={`id: ${car.id}`} />
+                        <Chip size="medium" label={`Miles_per_Gallon: ${car.Miles_per_Gallon}`} />
+                        <Chip size="medium" label={`Cylinders: ${car.Cylinders}`} />
+                        <Chip size="medium" label={`Displacement: ${car.Displacement}`} />
+                        <Chip size="medium" label={`Horsepower: ${car.Horsepower}`} />
+                        <Chip size="medium" label={`Weight_in_lbs: ${car.Weight_in_lbs}`} />
+                        <Chip size="medium" label={`Acceleration: ${car.Acceleration}`} />
+                        <Chip size="medium" label={`Year: ${car.Year}`} />
+                        <Chip size="medium" label={`Origin: ${car.Origin}`} />
+                    </div>
+                </Paper>
+            </Container>
+        </div>
     )
 }
 
-export default Car
+export default Car;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import { Card, CardContent, CardActions, Divider } from '@material-ui/core'
-import cars from '../cars.json'
+import React from "react";
+import { Card, CardContent, CardActions, Divider } from "@material-ui/core";
+import cars from "../cars.json";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    console.log('CARS', cars)
+   
     return (
         <div className="card-container">
-            {cars.map((car, idx) => (
-                <Card key={idx} className="card">
+            {cars.map((car, index) => (
+                <Card key={index} className="card">
                     <CardContent className="text-gray">
-                        <span>{car.Name.toUpperCase()}</span>
                         <ul>
-                        <li>Miles_per_Gallon: {car["Miles_per_Gallon"]}</li>
-                        <li>Cylinders: {car["Cylinders"]}</li>
-                        <li>Displacement: {car["Displacement"]}</li>
-                        <li>Horsepower: {car["Horsepower"]}</li>
+                            <li>Name: {car["Name"]}</li>
+                            <li>Miles_per_Gallon: {car["Miles_per_Gallon"]}</li>
+                            <li>Cylinders: {car["Cylinders"]}</li>
+                            <li>Displacement: {car["Displacement"]}</li>
+                            <li>Horsepower: {car["Horsepower"]}</li>
                         </ul>
                     </CardContent>
-                    <Divider />
-                    <CardActions style={{ color: 'mediumblue' }}>
-                        {/* Change a tag to Link */}
-                        <a>See more Details</a>
-                    </CardActions>
+                <Divider />
+            <CardActions style={{ color: "mediumblue" }}>
+                 <Link to={`/car/${car.id}`}>See More Details</Link>
+                       </CardActions>
                 </Card>
             ))}
         </div>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
